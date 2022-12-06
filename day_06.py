a982c8fbc9c04308bccfcc8042f436b129f0e9a7
Jon Hoffman <jon@findanomaly.com>
2022-12-06 12:51:03 +0000

    day 6 part 2

--- day_06.py
+++ day_06.py
@@ -15,3 +15,14 @@ def part1(buffer: str) -> int:
 
 assert part1(example) == 7
 print(part1(input))
+
+
+def part2(buffer: str) -> int:
+    for i in range(0, len(buffer)):
+        sub = buffer[i : i + 14]
+        if len(set(sub)) == 14:
+            return i + 14
+
+
+assert part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 19
+print(part2(input))
