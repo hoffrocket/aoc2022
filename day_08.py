c29aaf2115ae19f7d02607750c3fc174c9c3e448
Jon Hoffman <jon@findanomaly.com>
2022-12-08 13:39:44 +0000

    day 8 part 2

--- day_08.py
+++ day_08.py
@@ -37,3 +37,35 @@ def part1(rows: List[str]) -> int:
 
 assert part1(example) == 21, part1(example)
 print(part1(input))
+
+
+def part2(rows: List[str]) -> int:
+    def count_eaves(current_tree: int, l: List[int]):
+        count = 0
+        for i in l:
+            if i >= current_tree:
+                return count + 1
+            else:
+                count += 1
+        return count
+
+    grid = [list(map(int, l.strip())) for l in rows]
+    height = len(grid)
+    width = len(grid[0])
+    max_vis_score = 0
+    for y in range(0, height):
+        for x in range(0, width):
+            current_tree = grid[y][x]
+            vis_score = (
+                count_eaves(current_tree, reversed(grid[y][0:x]))
+                * count_eaves(current_tree, grid[y][x + 1 : width])
+                * count_eaves(current_tree, reversed([grid[i][x] for i in range(0, y)]))
+                * count_eaves(current_tree, [grid[i][x] for i in range(y + 1, height)])
+            )
+            max_vis_score = max(max_vis_score, vis_score)
+
+    return max_vis_score
+
+
+assert part2(example) == 8, part2(example)
+print(part2(input))
