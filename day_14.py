e83b9b3c6e983604edde942664912a08912a3fb1
Jon Hoffman <jon@findanomaly.com>
2022-12-14 16:47:00 +0000

    day 14 part 2

--- day_14.py
+++ day_14.py
@@ -1,6 +1,7 @@
 import functools
 import itertools
 import operator
+from collections import defaultdict
 from typing import List
 
 import aoc
@@ -11,8 +12,7 @@ example = """498,4 -> 498,6 -> 496,6
 503,4 -> 502,4 -> 502,9 -> 494,9""".splitlines()
 
 
-def part1(lines: List[str]) -> int:
-    source = tuple([500, 0])
+def parse_rocks(lines: List[str]):
     paths = [[tuple(map(int, point_str.split(","))) for point_str in line.strip().split(" -> ")] for line in lines]
     # print(paths)
     rocks = set()
@@ -24,7 +24,12 @@ def part1(lines: List[str]) -> int:
                 rocks.add(tuple([src[0], y]))
             for x in range(min(src[0], dest[0]), max(src[0], dest[0]) + 1):
                 rocks.add(tuple([x, src[1]]))
-    # print(rocks)
+    return rocks
+
+
+def part1(lines: List[str]) -> int:
+    source = tuple([500, 0])
+    rocks = parse_rocks(lines)
     sands = set()
     all_items = set(rocks)
 
@@ -64,9 +69,59 @@ aoc.assert_equals(24, part1(example))
 print(part1(input))
 
 
-# def part2(lines:  List[str]) -> int:
-#     return 0
+def part2(lines: List[str]) -> int:
+    source = tuple([500, 0])
+    rocks = parse_rocks(lines)
+    # print(rocks)
+    sands = set()
+    all_items = set(rocks)
+
+    max_y = max(p[1] for p in rocks) + 2
+    # print(max_y)
+    ys_dict = defaultdict(set)
+    for x, y in rocks:
+        ys_dict[x].add(y)
+
+    # print(max_y)
+    # print(ys_dict)
+    def get_rest_point(x, y):
+        all_items.add((x, max_y))
+        ys_dict[x].add(max_y)
+        # print(x, y, ys_dict)
+        try:
+            min_y = min(ys for ys in ys_dict.get(x) if ys > y)
+            return (x, min_y - 1)
+        except:
+            return None
+
+    sx, sy = source
+
+    loop_count = 100_000_000
+    while loop_count:
+        loop_count -= 1
+        rest_point = get_rest_point(sx, sy)
+        if not rest_point:
+            sx, sy = source
+        else:
+            # print("rest point", rest_point)
+            sx, sy = rest_point
+            if (sx - 1, sy + 1) not in all_items:
+                sx = sx - 1
+                sy = sy + 1
+            elif (sx + 1, sy + 1) not in all_items:
+                sx = sx + 1
+                sy = sy + 1
+            else:
+                # print("adding", sx, sy)
+                sands.add((sx, sy))
+                all_items.add((sx, sy))
+                ys_dict[sx].add(sy)
+                if (sx, sy) == source:
+                    break
+                sx, sy = source
+    # print(sands)
+    return len(sands)
 
 
-# aoc.assert_equals(140, part2(example))
-# print(part2(input))
+aoc.assert_equals(93, part2(example))
+print(part2(input))
